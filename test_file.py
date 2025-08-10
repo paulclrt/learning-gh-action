18be0747aa7fbb5aa0abd895c41715ce3fab78da
Paul Claret <p4ul.claret@gmail.com>
2025-08-10 17:07:49 +0200

    Deploying new features to prod (#8)
    * adding dev workflow + commenting failing test
    
    * adding artifacts for virutal env so tests can work
    
    coool
    
    * changing .venv to venv so artifacts works
    
    * fix: removed .venv in source activate
    
    * fix: making activate venv and run pytest in the same shell
    
    * switching artifacts for cache
    
    * forgot to delete artifact in test job
    
    * fixing cache
    
    Pulling the cache instead of waiting
    Reorganazing the running of test and recreating venv
    Caching pip dep instead of venv folder
    
    * fix runner.os
    
    * azdazda
    
    * adding matrix testing for multiple python version + saving output of tests
    
    * adding new test + correcting matrix error
    
    * making new tests
    
    this is un purpose not on workflow-sandbox to test if i can make changes
    to dev, merge my workflow into dev and not affect files
    
    * Fixing workflow matrix test + adding better output capture for tests (#1)
    
    * adding new workflow sandbox for testing + fixing pip and python version in matrix
    
    * making pytest more verbose during test
    
    disabled capturing of output
    enabled very verbose output
    added traceback in fail case
    
    * fixing bad tests (#2)
    
    * Workflow sandbox (#5)
    
    * adding new workflow sandbox for testing + fixing pip and python version in matrix
    
    * making pytest more verbose during test
    
    disabled capturing of output
    enabled very verbose output
    added traceback in fail case
    
    * adding check for workflow deployment (#3)
    
    Do not deploy when not dev branch
    
    * Fix/workflow donotdeploy (#4)
    
    * adding check for workflow deployment
    
    Do not deploy when not dev branch
    
    * fix double quote error
    
    * adding pr testing
    
    * adding hello world feature (#7)
    
    you passed the test :)

--- test_file.py
+++ test_file.py
@@ -1,6 +1,24 @@
-
+import requests
 
 def test_important():
+    """
+    THis is a shit test to learn workflows
+    """
+    assert 1+1 == 2
+
+def test_win():
+    """
+    THis is a shit test to learn workflows
+    """
+    assert 1+1 == 2
+
+def test_works():
+    """
+    THis is a shit test to learn workflows
+    """
+    assert 1+1 == 2
+
+def test_fine():
     """
     THis is a shit test to learn workflows
     """
@@ -11,4 +29,12 @@ def test_ohhhh_nooooo_this_fails():
     """
     What a shitty repo this is
     """
-    assert 1+1 == 3
+    assert 2+1 == 3
+
+
+def test_request_fails():
+    """
+    What a shitty repo this is
+    """
+    req = requests.get("http://paul-claret.pro/zaduazhdaz")
+    assert req.status_code == 404
