456ea869df74cf0ac0932a03a75d895c72f4b662
paulclrt <p4ul.claret@gmail.com>
2025-08-10 13:49:50 +0200

    adding a failing test just to see
    this is for learning purposes

--- test_file.py
+++ test_file.py
@@ -5,3 +5,10 @@ def test_important():
     THis is a shit test to learn workflows
     """
     assert 1+1 == 2
+
+
+def test_ohhhh_nooooo_this_fails():
+    """
+    What a shitty repo this is
+    """
+    assert 1+1 == 3
